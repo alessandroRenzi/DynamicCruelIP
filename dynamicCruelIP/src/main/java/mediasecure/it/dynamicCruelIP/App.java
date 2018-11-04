28f226d2b8e77cde72c3de811a3bd995171531de
Alessandro Renzi <alessandro.renzi26@gmail.com>
2018-11-04 19:10:46 +0100

    Created MavenProject
    Initial project commit

---
+++ dynamicCruelIP/src/main/java/mediasecure/it/dynamicCruelIP/App.java
@@ -0,0 +1,13 @@
+package mediasecure.it.dynamicCruelIP;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
